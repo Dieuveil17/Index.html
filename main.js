0e157bef32560dd248632bda47cf04a520cc1858
Dieuveil17 <dieuveiljonathan5@gmail.com>
2025-04-12 22:23:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", { 
+    strings:[" Createur des sites" , " Conception des sites" , " Reseau informatique " , " Conception Graphique" , " chauffeur ", " Vendeur " , " VB.Net "],
+    typespeed:100,
+    Backspeed:100,
+    backselay:1000,
+    loop:true,
+ });
